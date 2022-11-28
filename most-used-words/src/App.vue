7d2e233cc4088e2f7b045b725a6da01eaaf02b3c
NikolasDaltro <nikolasdsantos@gmail.com>
2022-11-28 10:25:25 -0300

    fim do projeto

--- most-used-words/src/App.vue
+++ most-used-words/src/App.vue
@@ -10,23 +10,14 @@
 </template>
 
 <script>
-import Home from "./components/Home.vue";
-
+import Home from "./components/Home";
 export default {
   name: "App",
   components: {
-    Home,
+    Home
   },
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,57 +1,50 @@
 <template>
   <v-container fluid>
-    <v-form>
-      <v-file-input
-        label="Selecione as Legendas"
-        prepend-icon="mdi-message-text"
-        append-outer-icon="mdi-send"
-        outlined
-        mutiple
-        chips
-        v-model="files"
-      @clcik:append-outer="processSubtitles" />
-    </v-form>
-    <div class="pills">
-      <Pill
-        v-for="word in groupedWords"
-        :key="word.name"
-        :name="word.name"
-        :amount="word.amount"
-      />
-    </div>
+      <v-form>
+          <v-file-input 
+            label="Selecione as Legendas"
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple
+            chips
+            v-model="files"
+            @click:append-outer="processSubtitles" />
+      </v-form>
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name"
+            :name="word.name" :amount="word.amount" />
+      </div>
   </v-container>
 </template>
 
 <script>
-import {ipcRenderer} from 'electron'
-import Pill from "./Pill.vue";
-
+import { ipcRenderer } from  'electron'
+import Pill from './Pill'
 export default {
-  components: { Pill },
-  data: function () {
-    return {
-      files: [],
-      groupedWords: [],
-  
-    };
-  },
-  methods: {
-    processSubtitles() {
-      const paths = this.files.map(f => f.path)
-      ipcRenderer.send('process-subtitles', this.files)
-      ipcRenderer.on('process-subtitles', (event, resp) => {
-      this.groupedWords = resp
-      })
-
+    components: { Pill },
+    data: function () {
+        return {
+            files: [],
+            groupedWords: []
+        }
     },
-  },
-};
+    methods: {
+        processSubtitles() {
+            const paths = this.files.map(f => f.path)
+            ipcRenderer.send('process-subtitles', paths)
+            ipcRenderer.on('process-subtitles', (event, resp) => {
+                this.groupedWords = resp
+            })
+        }
+    }
+}
 </script>
 
 <style>
-.pills {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
 </style>
